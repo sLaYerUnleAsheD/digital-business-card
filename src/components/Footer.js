05897fd5159f4c38c1a9406fabb2f1a721513136
mihirpanchal2603@gmail.com <mihirpanchal2603@gmail.com>
2022-05-16 20:31:52 +0530

    deploy app to github pages

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+const Footer = () => {
+    return (
+        <>
+            <div className="icons">
+                <button className="twitter btn"><i className="fa-brands fa-twitter"></i></button>
+                <button className="facebook btn"><i className="fa-brands fa-facebook-square"></i></button>
+                <button className="instagram btn"><i className="fa-brands fa-instagram"></i></button>
+                <button className="reddit btn"><i className="fa-brands fa-reddit-alien"></i></button>
+            </div>
+        </>
+    )
+}
+export default Footer
---
+++ src/components/Mid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "./About"
+import Interests from "./Interests"
+const Mid = () => {
+    return (
+        <div className="mid">
+            <About />
+            <Interests />
+        </div>
+    );
+}
+export default Mid;
